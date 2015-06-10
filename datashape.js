b774716f52f48d31168d5230b76db90b57966276
Drew Easley <DEasley@rallydev.com>
2015-06-10 14:06:52 -0600

    Updated to include new helper functions

--- datashape.js
+++ datashape.js
@@ -1,20 +1,133 @@
 var _ = require('lodash');
 
+function stats(number, sub)
+{
+        number = parseInt(number);
 
-function getDataShape(obj, sub) {
+        if (!sub) { sub = {sum:0, count:0, avg:0, max :number, min:number };}
+
+
+        sub.sum = sub.sum + number;
+        sub.count++;
+        sub.avg = (sub.sum / sub.count);
+        if (number > sub.max) { sub.max = number;}
+        if (number < sub.min) { sub.min = number;}
+        return sub;
+
+
+}
+
+function first(value, sub) {
+  if (!sub) { sub = value; }
+  return sub;
+}
+
+function last(value, sub) {
+  sub = value;
+  return sub;
+}
+
+function datestats(value, sub) {
+
+  if (!sub) { sub = {datestats: true, nulls:0, count:0, max :null, min:null };}
+  sub.count++;
+  if (value)
+  {
+  value = new Date(value);
+  
+    //value = new Date(value);
+    if (sub.max === null) { sub.max = value;}
+    if (sub.min === null) { sub.min = value;}
+
+    if (value > sub.max) { sub.max = value;}
+    if (value < sub.min) { sub.min = value;}
+
+  }
+
+  else {
+    sub.nulls++;
+  }
+
+
+
+  return sub;
+
+
+
+}
+
+function stringstats(value, sub) {
+  number = value.length || -1;
+  if (!sub) { sub = {stringstats: true, sum:0, nulls:0, count:0, avg:0, max :number, min:number };}
+
+
+  if (number == -1) { sub.nulls++; }
+
+  else {
+    sub.sum = sub.sum + number;
+    sub.count++;
+    sub.avg = (sub.sum / sub.count);
+    if (number > sub.max) { sub.max = number;}
+    if (number < sub.min) { sub.min = number;}
+  }
+
+  return sub;
+}
+
+function objtype(value, sub) {
+     value = ({}).toString.call(value).match(/\s([a-zA-Z]+)/)[1].toLowerCase();
+
+     if (!sub) { sub = {} }; //declare an empty sub
+
+     sub[value] = sub[value] || 0;
+     sub[value]++;
+     return sub;
+}
+
+
+function recursionPolice(recurseLevel, maxLevel) {
+    recurseLevel = recurseLevel || 0;
+    maxLevel = maxLevel || 50;
+
+    recurseLevel++;
+    if (recurseLevel > maxLevel) {
+      throw new Error('Recursion depth has been exceeded');
+    }
+
+    return recurseLevel;
+
+}
+
+function getDataShape(obj, sub, nestlevel) {
+
+        nestlevel = recursionPolice(nestlevel, 50); //Throw an exception if we exceed depth of 50
+
         var keyValues = Object.keys(obj);
 
+
+
+
+
         for (var ki in keyValues) {
+
             var kName = keyValues[ki];
 
+            if (_.isArray(obj[kName])) {
+              for (var i in obj[kName]) {
+                getDataShape(obj[kName][i], sub[kName], nestlevel);
+              }
+              //What to do if we encounter an array
+            }
 
-
-            if (_.isObject(obj[kName])) {
+            else if (_.isObject(obj[kName])) {
+                //what to do if we encounter a sub-object
                 var subObj = obj[kName];
 
                 if (!sub[kName]) { sub[kName] = {};}
-                getDataShape(subObj, sub[kName]);
+                getDataShape(subObj, sub[kName], nestlevel);
             }
+
+
 
             else {
 
@@ -23,6 +136,7 @@ function getDataShape(obj, sub) {
 
 
                 if (_.isNumber(sub[kName])) {
+                  //The shape template was populated with an integer. Let's count all the things
                     sub[kName]++;
                 }
 
@@ -75,4 +189,13 @@ function getDataShape(obj, sub) {
 }
 
 
-module.exports = getDataShape;
+module.exports = {
+  datashape: getDataShape,
+  stats: stats,
+  objtype: objtype,
+  stringstats: stringstats,
+  datestats: datestats,
+  first: first,
+  last:last
+}
+;
